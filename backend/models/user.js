0c25c460d8c054acb3644c7a013cb8a59b427640
ling <lingchen94tc@gmail.com>
2018-09-03 00:23:53 +1000

    implement register and authenticate in backend

---
+++ backend/models/user.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+// User Schema
+const UserSchema = mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+    posts: [{ _id: mongoose.Schema.Types.ObjectId, default: [] }]
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+/** get user by id;*/
+module.exports.getUserById = function (id, callback) {
+    User.findById(id, callback);
+}
+
+/** get user by username */
+module.exports.getUserByUsername = function (username, callback) {
+    const query = { username: username }
+    User.findOne(query, callback);
+}
+
+/** add a new user into database */
+module.exports.addUser = function (newUser, callback) {
+    // salt and hash password
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+}
+
+// compare user entered password to encrypted password stored in the data
+module.exports.comparePassword = function (candidatePassword, hash, callback) {
+    bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+        if (err) throw err;
+        callback(null, isMatch);
+    });
+}
---
+++ backend/routes/users-route.js
@@ -0,0 +1,70 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import passport from "passport";
+import User from "../models/user";
+import config from "../config/db"
+
+const userRouter = express.Router();
+
+// register
+userRouter.post('/register', (req, res, next) => {
+    // create a new user from form request
+    let newUser = new User({
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if (err) {
+            res.json({ success: false, msg: 'Failed to register user' });
+        } else {
+            res.json({ success: true, msg: 'User registered' });
+        }
+    });
+});
+
+//Authenticate
+userRouter.post('/authenticate', (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //get user by username
+    User.getUserByUsername(username, (err, user) => {
+        if (err) throw err;
+        if (!user) {
+            return res.json({ success: false, msg: 'User not found' });
+        }
+        // for found user, comparing its password to the encrypted password in the database
+        User.comparePassword(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+            if (isMatch) {
+                //generate token for user with correct password
+                const token = jwt.sign({ data: user }, config.secret, {
+                    expiresIn: 3600 // 1 hour
+                });
+                // send response in jason format
+                res.json({
+                    success: true,
+                    token: 'JWT ' + token,
+                    user: {
+                        id: user._id,
+                        username: user.username,
+                        email: user.email,
+                        posts: user.posts
+                    }
+                })
+            } else {
+                return res.json({ success: false, msg: 'Wrong password' });
+            }
+        });
+    });
+});
+
+//profile
+userRouter.get('/profile', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    res.json({ user: req.user });
+});
+
+module.exports = userRouter;
+
