a7920a61c95e7565c153a9bdb7f45cc9262a602c
ling <lingchen94tc@gmail.com>
2018-09-21 00:14:01 +1000

    Modify create post function

--- backend/models/user.js
+++ backend/models/user.js
@@ -22,6 +22,16 @@ module.exports.getUserByUsername = function (username, callback) {
     User.findOne(query, callback);
 }
 
+/** add post id to posts column when user create a new post */
+module.exports.updatePosts = function (username, id, callback) {
+    const postId = { _id: id };
+    User.findOneAndUpdate(
+        { username: username },
+        { $push: { posts: postId } },
+        callback
+    );
+}
+
 /** add a new user into database */
 module.exports.addUser = function (newUser, callback) {
     // salt and hash password
@@ -34,7 +44,7 @@ module.exports.addUser = function (newUser, callback) {
     });
 }
 
-// compare user entered password to encrypted password stored in the data
+/** compare user entered password to encrypted password stored in the data */
 module.exports.comparePassword = function (candidatePassword, hash, callback) {
     bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
         if (err) throw err;
--- backend/routes/posts-route.js
+++ backend/routes/posts-route.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Post from "../models/post";
+import User from "../models/user";
 
 
 const postRouter = express.Router();
@@ -22,22 +23,44 @@ postRouter.route("/posts/:id").get((req, res) => {
 
 // route for add a new post
 postRouter.route("/posts/add").post((req, res) => {
-    let post = new Post(req.body);
-    post
+    // create a new post by using data form the request body
+    let newPost = new Post({
+        title: req.body.title,
+        street: req.body.street,
+        suburb: req.body.suburb,
+        city: req.body.city,
+        state: req.body.state,
+        postCode: req.body.postCode,
+        roomType: req.body.roomType,
+        contactName: req.body.contactName,
+        phone: req.body.phone,
+        rent: req.body.rent,
+        byUser: req.body.byUser,
+        postDate: req.body.postDate,
+        description: req.body.description
+    });
+
+    newPost
         .save()
-        .then(post => {
-            res.json({ post: "Added successfully!" });
+        .then(newPost => {
+            User.updatePosts(newPost.byUser, newPost._id, (err) => {
+                if (err) {
+                    res.json({ success: false, msg: "Failed to add postid in the user posts column", log: err })
+                } else {
+                    res.json({ success: true, msg: "Added successfully!" });
+                }
+            })
         })
         .catch(err => {
-            res.send("Failed to create new post");
+            res.json({ success: false, msg: "Failed to create new post", log: err });
         });
 });
 
 // route for delete a post
 postRouter.route("/posts/delete/:id").get((req, res) => {
     Post.findByIdAndRemove({ _id: req.params.id }, (err, post) => {
-        if (err) res.json(err);
-        else res.json("Remove successfully");
+        if (err) res.json({ success: true, msg: "Failed to delete post", log: err });
+        else res.json({ success: true, msg: "Remove successfully" });
     });
 });
 
